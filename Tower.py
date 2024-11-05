0a20c7b686fca780d5d87a1bd44ead566efd61c0
VladislavDevs <supermars5327@gmail.com>
2024-11-06 06:41:05 +0700

    Домашнее задание

--- Tower.py
+++ Tower.py
@@ -6,32 +6,32 @@
 '''end_nod - стержень куда кладется диск'''
 '''mem_nod - промежуточный стержень'''
 '''count - счетчик шагов'''
-def hanoi_tower(disks: int, rods_list : list, start_rod : list, end_rod : list, mem_rod : list, count : list):
+def hanoi_tower(disks: int, rods_list : list, start_rod : list, end_rod : list, mem_rod : list, default_rod_list : list, start_rod_index : int, end_rod_index :int, count : list):
 
     if disks == 0:
         return
 
     if len(rods_list) == 3:
-        hanoi_tower(disks - 1, rods_list, start_rod, mem_rod, end_rod, count)
+        hanoi_tower(disks - 1, rods_list, start_rod, mem_rod, end_rod, default_rod_list, default_rod_list.index(start_rod), default_rod_list.index(mem_rod), count)
 
-        move_disk(disks, start_rod, end_rod, count)
+        move_disk(disks, start_rod, end_rod, default_rod_list.index(start_rod), default_rod_list.index(end_rod), count)
 
-        hanoi_tower(disks - 1, rods_list, mem_rod, end_rod, start_rod, count)
+        hanoi_tower(disks - 1, rods_list, mem_rod, end_rod, start_rod, default_rod_list, default_rod_list.index(mem_rod), default_rod_list.index(end_rod), count)
     else:
-        hanoi_tower(disks - 1, rods_list[:-1], start_rod, rods_list[-1], end_rod, count)
+        hanoi_tower(disks - 1, rods_list[:-1], start_rod, rods_list[-1], end_rod, default_rod_list, default_rod_list.index(start_rod), default_rod_list.index(rods_list[-1]), count)
 
-        move_disk(disks, start_rod, end_rod, count)
+        move_disk(disks, start_rod, end_rod, default_rod_list.index(start_rod), default_rod_list.index(end_rod), count)
 
-        hanoi_tower(disks - 1, rods_list[:-1], rods_list[-1], end_rod, start_rod, count)
+        hanoi_tower(disks - 1, rods_list[:-1], rods_list[-1], end_rod, start_rod, default_rod_list, default_rod_list.index(rods_list[-1]), default_rod_list.index(end_rod), count)
 
 '''Перемещение диска'''
 '''disk - диск который нужно переместить'''
 '''from_rod - с какого стержня'''
 '''to_rod - на какой стержнь'''
 '''count - счетчик шагов'''
-def move_disk(disk : int, from_rod : list, to_rod : list, count: list):
+def move_disk(disk : int, from_rod : list, to_rod : list, from_rod_index : int, to_rod_index : int, count: list):
     to_rod.append(from_rod.pop())
-    row = f"Блин {disk}: Стержень {str(from_rod)} -> Стержень {str(to_rod)}"
+    row = f"Блин {disk}: Стержень {str(from_rod_index)} -> Стержень {str(to_rod_index)}"
     print(row)
     count.append(disk)
     save_row_to_file(row)
@@ -72,12 +72,12 @@ def main():
             print("Количество стержней не может быть меньше 3")
             return 0
 
-        rods_dictionary = init_rods(int(disks), int(rods))
+        rods_list = init_rods(int(disks), int(rods))
 
         if int(disks) == 1:
             print(f"Блин 1: Стержень 1 -> Стержень 2")
         else:
-            hanoi_tower(int(disks), rods_dictionary, rods_dictionary[1], rods_dictionary[-1], rods_dictionary[2], count)
+            hanoi_tower(int(disks), rods_list, rods_list[1], rods_list[-1], rods_list[2], rods_list, rods_list.index(rods_list[1]), rods_list.index(rods_list[-1]), count)
             print(f"Всего шагов: {len(count)}")
     else:
         print("Введены неккоректные данные.")
